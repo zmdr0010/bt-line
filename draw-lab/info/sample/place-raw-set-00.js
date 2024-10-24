6606e9bf15e4fa74e2bb5c2f98b32d51ac9f94f2
codeidzmdr <codeidzmdr@gmail.com>
2024-10-24 10:52:38 +0900

    lab: save, load raw transform place

---
+++ draw-lab/info/sample/place-raw-set-00.js
@@ -0,0 +1,4 @@
+// uCode/num/rawSet uCode,x,y,szw,szh/rawSet uCode,x,y,szw,szh/ ... list
+const placeRawSet00List = [
+  'place-raw-20241024102852/2/raw-20241024102748,60,80,10,10/raw-20241024102553,100,120,20,20',
+]
