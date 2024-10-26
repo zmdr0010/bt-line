d2551b68d4ee25d89318c87523e007f11b49f3de
codeidzmdr <codeidzmdr@gmail.com>
2024-10-26 09:48:35 +0900

    lab: add dvc srt bundle list

---
+++ draw-lab/info/sample/bundle-set-00.js
@@ -0,0 +1,14 @@
+const bundleSet00List = [
+  'dvc-srt-tcode-bundle-20241025150806\n' +
+  'tCode\n' +
+  'c/3/cI0,cI1,cI2/2/cI0_cI1,cI0_cI2',
+
+  'dvc-srt-bundle-20241025151307\n' +
+  'edit\n' +
+  'dvc-srt-part-20241025150808/cI0/part/line-20241010120528/0//0/2/cI0,cI0_cI1,g,81,70/cI0,cI0_cI2,g,46,-12/0\n' +
+  'dvc-srt-part-20241025150808/cI1/part/line-20241013154104/0//0/1/cI1,cI0_cI1,m,27,70/0\n' +
+  'dvc-srt-part-20241025150808/cI2/part/line-20241017105015/0//0/1/cI2,cI0_cI2,m,77,39/1/scale,0.68,0.89\n' +
+  'dvc-srt-device-20241025150808/c/device//3/cI0,cI1,cI2/4/cI0,cI0_cI1,g,0,0/cI1,cI0_cI1,m,0,0/cI0,cI0_cI2,g,0,0/cI2,cI0_cI2,m,0,0/0/0',
+
+  // '',
+]
