e48032d605848abe65f12783a7caee2f44476549
codeidzmdr <codeidzmdr@gmail.com>
2024-11-14 15:54:35 +0900

    add mdn srs set add

--- draw-lab/info/sample/md-code-set-00.js
+++ draw-lab/info/sample/md-code-set-00.js
@@ -1,3 +1,3 @@
 // uCode/num/code,name,type/code,name,type/ ... list
 // type: mdn (mdn code) / ftr (ftr code)
-const mdCodeSet00 = 'md-code-00/4/md-prt-00,proto 00,mdn/md-prt-01,proto 01,mdn/ftr-hd,hd,ftr/ftr-mini,mini,ftr'
+const mdCodeSet00 = 'md-code-00/6/md-prt-00,proto 00,mdn/md-prt-01,proto 01,mdn/ftr-base,base,ftr/ftr-power,power,ftr/ftr-hd,hd,ftr/ftr-mini,mini,ftr'
--- draw-lab/info/sample/mdn-srs-set-00.js
+++ draw-lab/info/sample/mdn-srs-set-00.js
@@ -3,9 +3,9 @@
 // mdn full name: [mdn name] [(srs)nick name] [mnft year] [ftr]
 // filter -> mdn code -> srs list
 const mdnSrsSetList00 = [
-  'mdn-srs-prt-00-00/md-prt-00/proto 00/prt/1/base/2211/base',
-  'mdn-srs-prt-00-01/md-prt-00/proto 00/prt/2/alpha/2308/base',
-  'mdn-srs-prt-00-02/md-prt-00/proto 00/prt/3/beta/2311/base',
-  'mdn-srs-prt-00-03/md-prt-00/proto 00/prt/4/beta/2401/power',
-  'mdn-srs-prt-00-04/md-prt-00/proto 00/prt/5/alpha 2/2405/base'
+  'mdn-srs-prt-00-00/md-prt-00/proto 00/prt/1/base/2211/ftr-base/base',
+  'mdn-srs-prt-00-01/md-prt-00/proto 00/prt/2/alpha/2308/ftr-base/base',
+  'mdn-srs-prt-00-02/md-prt-00/proto 00/prt/3/beta/2311/ftr-base/base',
+  'mdn-srs-prt-00-03/md-prt-00/proto 00/prt/3/beta/2401/ftr-power/power',
+  'mdn-srs-prt-00-04/md-prt-00/proto 00/prt/4/alpha 2/2405/ftr-base/base'
 ]
